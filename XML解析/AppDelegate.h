00c78db123feb7d24d23dc1c8a7076373475ce55
wang yongfeng <wangyongfeng@wangmatoiMac.local>
2013-03-07 11:17:19 +0800

    Initial Commit

---
+++ XML解析/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  XML解析
+//
+//  Created by wang yongfeng on 13-3-7.
+//  Copyright (c) 2013年 wang yongfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ XML解析/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XML解析
+//
+//  Created by wang yongfeng on 13-3-7.
+//  Copyright (c) 2013年 wang yongfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
